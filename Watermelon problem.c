dc68af9aa6e45b78493023493c814457d01031b4
ARUNAV BISWAS <piankabir19997@gmail.com>
2026-03-12 22:55:35 +0600

    Add files via upload

---
+++ Watermelon problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int weight;
+    printf("Enter the weight of Watermelon:");
+    scanf("%d", &weight);
+    if (weight > 2 && weight%2==0)
+    printf("YES");
+    else
+    printf("NO");
+    return 0;
+}
